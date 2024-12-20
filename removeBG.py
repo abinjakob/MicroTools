606961b4be7f34d84d102fef78b197498145a737
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2024-12-20 11:46:42 +0100

    Add files via upload

---
+++ removeBG.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 11:08:24 2024
+
+@author: abinjacob
+"""
+
+# libraries 
+import os.path as op
+from rembg import remove
+from PIL import Image
+
+# file and folder
+folderpath = r'/Users/abinjacob/Documents/Fun Coding/resources'
+filename = 'sampleimage1.jpeg'
+loadpath = op.join(folderpath,filename)
+savepath = op.join(folderpath,f'{op.splitext(filename)[0]}_BGremoved.png')
+# opening image 
+img = Image.open(loadpath)
+# removing bacground
+output = remove(img)
+# save and open image 
+output.save(savepath)
+Image.open(savepath)
+
